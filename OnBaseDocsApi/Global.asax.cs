0d07e95b393c99841c09746f8af5e25fc59cc129
Miguel Fernandez <miguel.fernandez@oregonstate.edu>
2020-02-20 13:34:40 -0800

    Adding ASP.NET API skeleton.

---
+++ OnBaseDocsApi/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Http;
+
+namespace OnBaseDocsApi
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
