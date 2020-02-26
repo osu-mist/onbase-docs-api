d0fed3b3dca9783eff67eeb700c87deee78e7648
Miguel Fernandez <miguel.fernandez@oregonstate.edu>
2020-02-26 12:53:53 -0800

    Updating doc ID to long.

--- OnBaseDocsApi/Controllers/OnBaseDocsController.cs
+++ OnBaseDocsApi/Controllers/OnBaseDocsController.cs
@@ -20,7 +20,7 @@ namespace OnBaseDocsApi.Controllers
     {
         [HttpGet]
         [ActionName("")]
-        public IHttpActionResult Get(int id)
+        public IHttpActionResult Get(long id)
         {
             return TryHandleDocRequest(id, (_, doc) =>
             {
@@ -30,7 +30,7 @@ namespace OnBaseDocsApi.Controllers
 
         [HttpGet]
         [ActionName("File")]
-        public IHttpActionResult GetFile(int id)
+        public IHttpActionResult GetFile(long id)
         {
             return TryHandleDocRequest(id, (app, doc) =>
             {
@@ -200,9 +200,8 @@ namespace OnBaseDocsApi.Controllers
 
         void MoveDocumentToWorkflow(long docId, string documentType)
         {
-            TryHandleRequest(app =>
+            TryHandleDocRequest(docId, (app, doc) =>
             {
-                var doc = app.Core.GetDocumentByID(docId);
                 var docType = app.Core.DocumentTypes.Find(documentType);
 
                 // First index the document so that the autofill keywords are populated
@@ -215,7 +214,7 @@ namespace OnBaseDocsApi.Controllers
             });
         }
 
-        IHttpActionResult TryHandleDocRequest(int id,
+        IHttpActionResult TryHandleDocRequest(long id,
             Func<Application, Document, IHttpActionResult> handler)
         {
             return TryHandleRequest((app) =>
