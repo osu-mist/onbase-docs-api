747ff5c0230754a0b3b4de505060e8223ff847b8
Miguel Fernandez <miguel.fernandez@oregonstate.edu>
2020-02-26 10:31:45 -0800

    Adding support for document upload.

---
+++ OnBaseDocsApi/Models/DocumentCreateAttributes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnBaseDocsApi.Models
+{
+    public class DocumentPostAttributes
+    {
+        public string DocumentType { get; set; }
+        public string Comment { get; set; }
+        public string Name { get; set; }
+        public string IndexKey { get; set; }
+        public IEnumerable<Keyword> Keywords { get; set; }
+    }
+}
---
+++ OnBaseDocsApi/Models/DocumentPostAttributes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnBaseDocsApi.Models
+{
+    public class DocumentAttributes
+    {
+        public long CreatedBy { get; set; }
+        public DateTime DateStored { get; set; }
+        public DateTime DocumentDate { get; set; }
+        public string DocumentType { get; set; }
+        public string Status { get; set; }
+        public string Name { get; set; }
+        public long ID { get; set; }
+        public long LatestAllowedRevisionID { get; set; }
+        public string DefaultFileType { get; set; }
+        public IEnumerable<Keyword> Keywords { get; set; }
+    }
+}
